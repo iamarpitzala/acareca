f37fd8fb8f7205c0d088271f125fedcd85a28b28
rishitamZenithive <rishitam@zenithive.com>
2026-04-15 10:45:35 +0530

    chore(form): changed `form:"clinicId"` to `form:"clinicIds"`

--- internal/modules/builder/form/model.go
+++ internal/modules/builder/form/model.go
@@ -112,7 +112,7 @@ type RsFormWithFields struct {
 
 type Filter struct {
 	PractitionerID *string      `form:"practitioner_id"`
-	ClinicID       []*uuid.UUID `form:"clinicId"`
+	ClinicID       []*uuid.UUID `form:"clinicIds"`
 	FormName       *string      `form:"form_name"`
 	Method         *string      `form:"method"`
 	Status         *string      `form:"status"`
